aa43a35393decc7c95469b2433e3ee5cded8ca4a
Kai GAO <kaigaoinau@gmail.com>
2019-03-01 00:12:19 +1100

    Fri 1 Mar

---
+++ Easy/125.valid-palindrome.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (30.18%)
+ * Total Accepted:    321.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ * 
+ * Note: For the purpose of this problem, we define empty string as valid
+ * palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "race a car"
+ * Output: false
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const length = str.length;
+    const first = str.slice(0, Math.floor(length/2));
+    const second = str.split("").reverse().join("").slice(0, Math.floor(length/2));
+    if(first === second){
+      return true;
+    }
+    return false;
+};
+
